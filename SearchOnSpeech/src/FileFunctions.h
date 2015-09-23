37f692c3e0283ef1310e56d5067a0516c1fbc9cf
gtm-uvigo <gtm-uvigo@users.noreply.github.com>
2015-09-23 15:41:06 +0200

    Update FileFunctions.h

--- SearchOnSpeech/src/FileFunctions.h
+++ SearchOnSpeech/src/FileFunctions.h
@@ -1,3 +1,8 @@
+/*********************************************************************
+ *  Copyright (C) Paula López Otero, GTM, Universidade de Vigo,2015  *
+ *  E-mail: plopez@gts.uvigo.es                                      *
+ * *******************************************************************/
+
 Col<int> readKeepFile(char *dataFile);
 int getSamplePeriod(char *dataFile);
 Mat<float> readFile(char *dataFile);
